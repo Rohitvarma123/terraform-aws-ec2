93384cd83e8fa754b046bb6b47b3f1d0f0208b89
Krishnamohan2004 <krishnamohan6328@gmail.com>
2026-06-24 10:03:13 +0530

    terraform.tfvars

---
+++ Variable_block/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id       = "ami-0c1a7f89451184c8b"
+instance_type = "t3.micro"
+instance_name = "rayalaseem-super-fast-expression"
---
+++ Variable_block/variable.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "Amazon Linux AMI ID"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+}
+
+variable "instance_name" {
+  description = "EC2 Name"
+}
